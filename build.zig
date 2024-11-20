9b93adb845c94ecf4b38d20ed55f6d8e3d61a9f6
sdzx-1 <shangdizhixia1993@163.com>
2024-11-20 14:33:31 +0800

    Add example atm

--- build.zig
+++ build.zig
@@ -15,43 +15,43 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
-        .name = "typed-fsm-example",
-        .root_source_file = b.path("examples/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const @"typed-fsm" = @This().getModule(b, target, optimize);
-    exe.root_module.addImport("typed-fsm", @"typed-fsm");
-
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
-    b.installArtifact(exe);
-
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
-    const run_cmd = b.addRunArtifact(exe);
-
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
+    {
+        const exe = b.addExecutable(.{
+            .name = "counter",
+            .root_source_file = b.path("examples/counter.zig"),
+            .target = target,
+            .optimize = optimize,
+        });
+        const @"typed-fsm" = @This().getModule(b, target, optimize);
+        exe.root_module.addImport("typed-fsm", @"typed-fsm");
+        b.installArtifact(exe);
+        const run_cmd = b.addRunArtifact(exe);
+        run_cmd.step.dependOn(b.getInstallStep());
+        if (b.args) |args| {
+            run_cmd.addArgs(args);
+        }
+        const run_step = b.step("counter", "Run the app");
+        run_step.dependOn(&run_cmd.step);
     }
 
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
+    {
+        const exe = b.addExecutable(.{
+            .name = "atm",
+            .root_source_file = b.path("examples/atm.zig"),
+            .target = target,
+            .optimize = optimize,
+        });
+        const @"typed-fsm" = @This().getModule(b, target, optimize);
+        exe.root_module.addImport("typed-fsm", @"typed-fsm");
+        b.installArtifact(exe);
+        const run_cmd = b.addRunArtifact(exe);
+        run_cmd.step.dependOn(b.getInstallStep());
+        if (b.args) |args| {
+            run_cmd.addArgs(args);
+        }
+        const run_step = b.step("atm", "Run the app");
+        run_step.dependOn(&run_cmd.step);
+    }
 }
 
 fn getModule(
---
+++ examples/atm.zig
@@ -0,0 +1,192 @@
+const std = @import("std");
+const typedFsm = @import("typed-fsm");
+const Witness = typedFsm.Witness;
+
+pub fn main() !void {
+    var ist: InternalState = .{
+        .pin = 1234,
+        .times = 0,
+        .amount = 10000,
+        .buf = undefined,
+    };
+    const start = AtmSt.T(.ready){};
+    readyHander(start, &ist);
+}
+
+const AtmSt = enum {
+    exit,
+    ready,
+    cardInserted,
+    session,
+
+    pub fn T(s: AtmSt) type {
+        return Witness(AtmSt, .exit, s);
+    }
+    fn W(end: AtmSt, s: AtmSt) type {
+        return Witness(AtmSt, end, s);
+    }
+
+    pub fn exitMsg(_: AtmSt) type {
+        return void;
+    }
+
+    const input = std.io.getStdIn().reader();
+
+    pub fn readyMsg(end: AtmSt) type {
+        return union(enum) {
+            ExitAtm: W(end, .exit),
+            InsertCard: W(end, .cardInserted),
+
+            pub fn getMsg(buf: []u8) @This() {
+                while (true) {
+                    std.debug.print("insert or exit: ", .{});
+                    const res = input.readUntilDelimiterOrEof(buf, '\n') catch blk: {
+                        break :blk null;
+                    };
+                    if (res) |line| {
+                        if (std.mem.eql(u8, line, "insert")) {
+                            return .InsertCard;
+                        } else if (std.mem.eql(u8, line, "exit")) {
+                            return .ExitAtm;
+                        }
+                    }
+
+                    std.debug.print("input error\n", .{});
+                }
+            }
+        };
+    }
+
+    pub fn cardInsertedMsg(end: AtmSt) type {
+        return union(enum) {
+            Correct: W(end, .session),
+            Incorrect: W(end, .cardInserted),
+            EjectCard: W(end, .ready),
+
+            pub fn getMsg(buf: []u8, ist: *const InternalState) @This() {
+                while (true) {
+                    std.debug.print("input pin: ", .{});
+                    const res = input.readUntilDelimiterOrEof(buf, '\n') catch blk: {
+                        break :blk null;
+                    };
+                    if (res) |line| {
+                        const pres = std.fmt.parseInt(usize, line, 10) catch blk1: {
+                            break :blk1 null;
+                        };
+
+                        if (pres) |v| {
+                            if (ist.pin == v) {
+                                return .Correct;
+                            } else {
+                                if (ist.times == 3) return .EjectCard;
+                                return .Incorrect;
+                            }
+                        }
+                    }
+
+                    std.debug.print("input error\n", .{});
+                }
+            }
+        };
+    }
+
+    pub fn sessionMsg(end: AtmSt) type {
+        return union(enum) {
+            GetAmount: W(end, .session),
+            Disponse: struct { v: usize, wit: W(end, .session) = .{} },
+            EjectCard: W(end, .ready),
+
+            pub fn getMsg(buf: []u8, _: *const InternalState) @This() {
+                while (true) {
+                    std.debug.print("getAmount or disponse or eject: ", .{});
+                    const res = input.readUntilDelimiterOrEof(buf, '\n') catch blk: {
+                        break :blk null;
+                    };
+                    if (res) |line| {
+                        if (std.mem.eql(u8, line, "getAmount")) {
+                            return .GetAmount;
+                        } else if (std.mem.eql(u8, line, "eject")) {
+                            return .EjectCard;
+                        } else {
+                            const pres = std.fmt.parseInt(usize, line, 10) catch blk1: {
+                                break :blk1 null;
+                            };
+                            if (pres) |v| return .{ .Disponse = .{ .v = v } };
+                        }
+                    }
+
+                    std.debug.print("input error\n", .{});
+                }
+            }
+        };
+    }
+};
+
+const InternalState = struct {
+    pin: usize,
+    times: usize,
+    amount: usize,
+    buf: [100]u8,
+};
+
+// ready
+pub fn readyHander(comptime w: AtmSt.T(.ready), ist: *InternalState) void {
+    std.debug.print("ready\n", .{});
+    switch (w.getMsg()(&ist.buf)) {
+        .ExitAtm => |witness| {
+            std.debug.print("Exit ATM!\n", .{});
+            witness.terminal();
+        },
+        .InsertCard => |witness| {
+            ist.times = 0;
+            @call(.always_tail, cardInsertedHander, .{ witness, ist });
+        },
+    }
+}
+
+// cardInserted,
+pub fn cardInsertedHander(comptime w: AtmSt.T(.cardInserted), ist: *InternalState) void {
+    std.debug.print("cardInserted\n", .{});
+    switch (w.getMsg()(&ist.buf, ist)) {
+        .Correct => |wit| {
+            ist.times += 1;
+            std.debug.print("The pin correct, goto session!\n", .{});
+            @call(.always_tail, sessionHander, .{ wit, ist });
+        },
+        .Incorrect => |wit| {
+            ist.times += 1;
+            std.debug.print("The pin incorrect, goto cardInserted!\n", .{});
+            @call(.always_tail, cardInsertedHander, .{ wit, ist });
+        },
+        .EjectCard => |wit| {
+            std.debug.print("Test times great than 3, eject card!\n", .{});
+            @call(.always_tail, readyHander, .{ wit, ist });
+        },
+    }
+}
+
+// session,
+pub fn sessionHander(comptime w: AtmSt.T(.session), ist: *InternalState) void {
+    std.debug.print("session\n", .{});
+    switch (w.getMsg()(&ist.buf, ist)) {
+        .GetAmount => |wit| {
+            std.debug.print("amount: {d}\n", .{ist.amount});
+            @call(.always_tail, sessionHander, .{ wit, ist });
+        },
+
+        .Disponse => |val| {
+            if (ist.amount >= val.v) {
+                ist.amount -= val.v;
+                std.debug.print("disponse: {d}\n", .{val.v});
+                std.debug.print("new amount: {d}\n", .{ist.amount});
+            } else {
+                std.debug.print("insufficient balance\n", .{});
+            }
+            @call(.always_tail, sessionHander, .{ val.wit, ist });
+        },
+        .EjectCard => |wit| {
+            std.debug.print("eject card\n", .{});
+            @call(.always_tail, readyHander, .{ wit, ist });
+        },
+    }
+}
