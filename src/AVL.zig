f3c305f3ce55fe1d49edfca93e416bfac80568c3
sdzx-1 <shangdizhixia1993@163.com>
2025-06-30 16:59:15 +0800

    Impl AVL tree use array!

---
+++ src/AVL.zig
@@ -0,0 +1,277 @@
+//The AVL tree implemented using array allows up to 100,000 states.
+//https://www.geeksforgeeks.org/c/c-program-to-implement-avl-tree/
+//
+idx: i32 = 0,
+nodes: [100_000]Node = undefined,
+
+pub const Node = struct {
+    key: u32,
+    data: u32,
+    left: i32,
+    right: i32,
+    height: i32,
+};
+const Self = @This();
+
+pub fn search(self: *Self, root: i32, key: u32) ?u32 {
+    if (root == -1) return null;
+    const node = self.access(root);
+    if (key == node.key) return node.data;
+    if (key < node.key) return self.search(node.left, key);
+    if (key > node.key) return self.search(node.right, key);
+    return null;
+}
+pub fn insert(self: *Self, node: i32, key: u32, data: u32) i32 {
+    // // Function to insert a key into AVL tree
+    // struct Node* insert(struct Node* node, int key)
+    // {
+    //     // 1. Perform standard BST insertion
+    //     if (node == NULL)
+    //         return createNode(key);
+    if (node == -1) return self.createNode(key, data);
+
+    //     if (key < node->key)
+    //     else if (key > node->key)
+    //     else // Equal keys are not allowed in BST
+
+    if (key < self.access(node).key) {
+        //         node->left = insert(node->left, key);
+        self.access(node).left = self.insert(self.access(node).left, key, data);
+    } else if (key > self.access(node).key) {
+        //         node->right = insert(node->right, key);
+        self.access(node).right = self.insert(self.access(node).right, key, data);
+    } else {
+        //         return node;
+        return node;
+    }
+
+    //     // 2. Update height of this ancestor node
+    //     node->height = 1
+    //                    + max(getHeight(node->left),
+    //                          getHeight(node->right));
+
+    self.access(node).height = @as(i32, 1) + @max(
+        self.getHeight(self.access(node).left),
+        self.getHeight(self.access(node).right),
+    );
+
+    //     // 3. Get the balance factor of this ancestor node to
+    //     // check whether this node became unbalanced
+    //     int balance = getBalanceFactor(node);
+    const balance = self.getBalanceFactor(node);
+
+    //     // 4. If the node becomes unbalanced, then there are 4
+    //     // cases
+
+    //     // Left Left Case
+    //     if (balance > 1 && key < node->left->key)
+    //         return rightRotate(node);
+
+    if (balance > 1 and key < self.access(self.access(node).left).key) {
+        return self.rightRotate(node);
+    }
+
+    //     // Right Right Case
+    //     if (balance < -1 && key > node->right->key)
+    //         return leftRotate(node);
+
+    if (balance < -1 and key > self.access(self.access(node).right).key) {
+        return self.leftRotate(node);
+    }
+
+    //     // Left Right Case
+    //     if (balance > 1 && key > node->left->key) {
+    //         node->left = leftRotate(node->left);
+    //         return rightRotate(node);
+    //     }
+
+    if (balance > 1 and key > self.access(self.access(node).left).key) {
+        self.access(node).left = self.leftRotate(self.access(node).left);
+        return self.rightRotate(node);
+    }
+
+    //     // Right Left Case
+    //     if (balance < -1 && key < node->right->key) {
+    //         node->right = rightRotate(node->right);
+    //         return leftRotate(node);
+    //     }
+
+    if (balance < -1 and key < self.access(self.access(node).right).key) {
+        self.access(node).right = self.rightRotate(self.access(node).right);
+        return self.leftRotate(node);
+    }
+
+    //     // Return the (unchanged) node pointer
+    //     return node;
+    return node;
+}
+
+fn access(self: *Self, i: i32) *Node {
+    return &self.nodes[@as(usize, @intCast(i))];
+}
+
+// // Function to get height of the node
+// int getHeight(struct Node* n)
+// {
+//     if (n == NULL)
+//         return 0;
+//     return n->height;
+// }
+
+fn getHeight(self: *Self, n: i32) i32 {
+    if (n == -1) {
+        return 0;
+    } else {
+        return self.access(n).height;
+    }
+}
+
+// // Function to create a new node
+// struct Node* createNode(int key)
+// {
+//     struct Node* node
+//         = (struct Node*)malloc(sizeof(struct Node));
+//     node->key = key;
+//     node->left = NULL;
+//     node->right = NULL;
+//     node->height = 1; // New node is initially added at leaf
+//     return node;
+// }
+
+fn createNode(self: *Self, key: u32, data: u32) i32 {
+    const curr = self.idx;
+    self.idx += 1;
+    self.access(curr).* = .{
+        .key = key,
+        .data = data,
+        .left = -1,
+        .right = -1,
+        .height = 1,
+    };
+    return curr;
+}
+
+// // Utility function to get the maximum of two integers
+// int max(int a, int b) { return (a > b) ? a : b; }
+
+// // Function to get balance factor of a node
+// int getBalanceFactor(struct Node* n)
+// {
+//     if (n == NULL)
+//         return 0;
+//     return getHeight(n->left) - getHeight(n->right);
+// }
+
+fn getBalanceFactor(self: *Self, n: i32) i32 {
+    if (n == -1) return 0;
+    return self.getHeight(self.access(n).left) - self.getHeight(self.access(n).right);
+}
+
+fn rightRotate(self: *Self, y: i32) i32 {
+    // // Right rotation function
+    // struct Node* rightRotate(struct Node* y)
+    // {
+    //     struct Node* x = y->left;
+    const x = self.access(y).left;
+    //     struct Node* T2 = x->right;
+    const T2 = self.access(x).right;
+
+    //     // Perform rotation
+    //     x->right = y;
+    //     y->left = T2;
+    self.access(x).right = y;
+    self.access(y).left = T2;
+
+    //     // Update heights
+    //     y->height
+    //         = max(getHeight(y->left), getHeight(y->right)) + 1;
+    self.access(y).height = @max(
+        self.getHeight(self.access(y).left),
+        self.getHeight(self.access(y).right),
+    ) + @as(i32, 1);
+
+    //     x->height
+    //         = max(getHeight(x->left), getHeight(x->right)) + 1;
+
+    self.access(x).height = @max(
+        self.getHeight(self.access(x).left),
+        self.getHeight(self.access(x).right),
+    ) + @as(i32, 1);
+
+    //     return x;
+    return x;
+}
+
+fn leftRotate(self: *Self, x: i32) i32 {
+    // // Left rotation function
+    // struct Node* leftRotate(struct Node* x)
+    // {
+    //     struct Node* y = x->right;
+    const y = self.access(x).right;
+    //     struct Node* T2 = y->left;
+    const T2 = self.access(y).left;
+
+    //     // Perform rotation
+    //     y->left = x;
+    self.access(y).left = x;
+    //     x->right = T2;
+    self.access(x).right = T2;
+
+    //     // Update heights
+    //     x->height
+    //         = max(getHeight(x->left), getHeight(x->right)) + 1;
+
+    self.access(x).height = @max(
+        self.getHeight(self.access(x).left),
+        self.getHeight(self.access(x).right),
+    ) + @as(i32, 1);
+    //     y->height
+    //         = max(getHeight(y->left), getHeight(y->right)) + 1;
+
+    self.access(y).height = @max(
+        self.getHeight(self.access(y).left),
+        self.getHeight(self.access(y).right),
+    ) + @as(i32, 1);
+
+    //     return y;
+    return y;
+}
+
+// // Function to perform preorder traversal of AVL tree
+// void inOrder(struct Node* root)
+// {
+//     if (root != NULL) {
+//         inOrder(root->left);
+//         printf("%d ", root->key);
+//         inOrder(root->right);
+//     }
+// }
+
+fn inOrder(self: *Self, root: i32) void {
+    if (root != -1) {
+        self.inOrder(self.access(root).left);
+        std.debug.print("{d} ", .{self.access(root).key});
+        self.inOrder(self.access(root).right);
+    }
+}
+
+test "avl" {
+    var avl: @This() = .{};
+
+    var root: i32 = -1;
+    root = avl.insert(root, 2, 102);
+    root = avl.insert(root, 1, 101);
+    root = avl.insert(root, 5, 105);
+    root = avl.insert(root, 6, 106);
+    root = avl.insert(root, 3, 103);
+    root = avl.insert(root, 4, 104);
+
+    std.debug.assert(avl.search(root, 1) == 101);
+    std.debug.assert(avl.search(root, 2) == 102);
+    std.debug.assert(avl.search(root, 3) == 103);
+    std.debug.assert(avl.search(root, 4) == 104);
+    std.debug.assert(avl.search(root, 5) == 105);
+    std.debug.assert(avl.search(root, 6) == 106);
+}
+
+const std = @import("std");
--- src/polystate.zig
+++ src/polystate.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const Adler32 = std.hash.Adler32;
+const AVL = @import("AVL.zig");
 
 pub const Exit = union(enum) {};
 
@@ -13,79 +14,12 @@ pub fn FSM(
     enter_fn: ?fn (*context, type) void,
     state: type,
 ) type {
-    if (state == Exit) {
-        return struct {
-            pub const Name = name;
-            pub const Context = context;
-            pub const EnterFn = enter_fn;
-            pub const State = state;
-
-            pub inline fn handler(_: *Context) void {}
-
-            pub fn conthandler(_: *Context) ContResult(Context) {
-                return .exit;
-            }
-        };
-    } else {
-        return struct {
-            pub const Name = name;
-            pub const Context = context;
-            pub const EnterFn = enter_fn;
-            pub const State = state;
-
-            pub fn handler_normal(ctx: *Context) void {
-                if (enter_fn) |fun| fun(ctx, State);
-                switch (State.handler(ctx)) {
-                    inline else => |wit, tag| {
-                        _ = tag;
-                        const FSMState = @TypeOf(wit);
-                        checkConsistency(FSMState.Name, Name);
-                        @call(.auto, FSMState.handler, .{ctx});
-                    },
-                }
-            }
-
-            pub fn handler(ctx: *Context) void {
-                if (enter_fn) |fun| fun(ctx, State);
-                switch (State.handler(ctx)) {
-                    inline else => |wit, tag| {
-                        _ = tag;
-                        const FSMState = @TypeOf(wit);
-                        checkConsistency(FSMState.Name, Name);
-                        @call(.always_tail, FSMState.handler, .{ctx});
-                    },
-                }
-            }
-
-            pub fn conthandler(ctx: *Context) ContResult(Context) {
-                if (enter_fn) |fun| fun(ctx, State);
-                const contFun: fn (*Context) NextState(State) = State.conthandler;
-                switch (contFun(ctx)) {
-                    inline .no_trasition => return .no_trasition,
-                    inline .next => |wit0| {
-                        switch (wit0) {
-                            inline else => |wit, tag| {
-                                _ = tag;
-                                const FSMState = @TypeOf(wit);
-                                checkConsistency(FSMState.Name, Name);
-                                return .{ .next = FSMState.conthandler };
-                            },
-                        }
-                    },
-                    inline .current => |wit0| {
-                        switch (wit0) {
-                            inline else => |wit, tag| {
-                                _ = tag;
-                                const FSMState = @TypeOf(wit);
-                                checkConsistency(FSMState.Name, Name);
-                                return .{ .current = FSMState.conthandler };
-                            },
-                        }
-                    },
-                }
-            }
-        };
-    }
+    return struct {
+        pub const Name = name;
+        pub const Context = context;
+        pub const EnterFn = enter_fn;
+        pub const State = state;
+    };
 }
 
 fn checkConsistency(comptime b: []const u8, comptime a: []const u8) void {
@@ -98,13 +32,28 @@ fn checkConsistency(comptime b: []const u8, comptime a: []const u8) void {
     }
 }
 
+pub fn run_handler(fsm_state: type, ctx: *fsm_state.Context) void {
+    const Name = fsm_state.Name;
+    const State = fsm_state.State;
+
+    if (State == Exit) return;
+    switch (State.handler(ctx)) {
+        inline else => |wit, tag| {
+            _ = tag;
+            const FSMState = @TypeOf(wit);
+            checkConsistency(FSMState.Name, Name);
+            run_handler(FSMState, ctx);
+            // continue :sw @call(.always_tail, FSMState.State.handler, .{ctx});
+        },
+    }
+}
+
 pub fn ContResult(context: type) type {
     const Context = context;
     return union(enum) {
         exit: void,
-        no_trasition: void,
+        no_trasition: *const fn (*Context) ContResult(Context),
         next: *const fn (*Context) ContResult(Context),
-        current: *const fn (*Context) ContResult(Context),
     };
 }
 
@@ -115,6 +64,49 @@ pub fn NextState(state: type) type {
         next: State,
         current: State,
     };
+}
+
+pub fn run_conthandler(fsm_state: type) fn (*fsm_state.Context) ContResult(fsm_state.Context) {
+    const tmp = struct {
+        pub fn fun(ctx: *fsm_state.Context) ContResult(fsm_state.Context) {
+            return run_conthandler_inner(fsm_state, ctx);
+        }
+    };
+    return tmp.fun;
+}
+
+pub fn run_conthandler_inner(fsm_state: type, ctx: *fsm_state.Context) ContResult(fsm_state.Context) {
+    const Context = fsm_state.Context;
+    const State = fsm_state.State;
+    const Name = fsm_state.Name;
+
+    if (fsm_state.State == Exit) return .exit;
+    if (fsm_state.EnterFn) |fun| fun(ctx, State);
+    const contFun: fn (*Context) NextState(State) = State.conthandler;
+    switch (contFun(ctx)) {
+        inline .no_trasition => return .{ .no_trasition = run_conthandler(fsm_state) },
+        inline .next => |wit0| {
+            switch (wit0) {
+                inline else => |wit, tag| {
+                    _ = tag;
+                    const FSMState = @TypeOf(wit);
+                    checkConsistency(FSMState.Name, Name);
+                    if (FSMState.State == Exit) return .exit;
+                    return .{ .next = run_conthandler(FSMState) };
+                },
+            }
+        },
+        inline .current => |wit0| {
+            switch (wit0) {
+                inline else => |wit, tag| {
+                    _ = tag;
+                    const FSMState = @TypeOf(wit);
+                    checkConsistency(FSMState.Name, Name);
+                    return run_conthandler_inner(FSMState, ctx);
+                },
+            }
+        },
+    }
 }
 
 pub const Graph = struct {
@@ -267,3 +259,81 @@ pub const Graph = struct {
         }
     }
 };
+
+pub const FSMStateCollect = struct {
+    id: usize = 0,
+    root: i32 = -1,
+    avl: AVL = .{},
+    fsm_states: [100_000]type = @splat(void),
+
+    const Self = @This();
+
+    pub fn collect_all(self: *Self, fsm_state: type) void {
+        if (self.avl.search(self.root, Adler32.hash(@typeName(fsm_state)))) |_| {
+            return;
+        } else {
+            self.fsm_states[self.id] = fsm_state;
+            self.root = self.avl.insert(self.root, Adler32.hash(@typeName(fsm_state)), self.id);
+            self.id += 1;
+
+            switch (@typeInfo(fsm_state.State)) {
+                .@"union" => |un| {
+                    inline for (un.fields) |field| {
+                        const next_fsm_state = field.type;
+                        self.collect_all(next_fsm_state);
+                    }
+                },
+                else => @compileError("Only support tagged union!"),
+            }
+        }
+    }
+};
+
+pub fn collect_all(fsm_state: type) FSMStateCollect {
+    @setEvalBranchQuota(10_000_000);
+    var fsm_state_collect: FSMStateCollect = .{};
+    fsm_state_collect.collect_all(fsm_state);
+    return fsm_state_collect;
+}
+
+pub fn Runner(fsm_state: type) type {
+    const Context = fsm_state.Context;
+    const fsm_state_collect = collect_all(fsm_state);
+
+    return struct {
+        pub const StateId = std.math.IntFittingRange(0, fsm_state_collect.id);
+
+        pub fn run_conthandler(curr_id: StateId, ctx: *Context) ?StateId {
+            sw: switch (curr_id) {
+                inline 0...fsm_state_collect.id - 1 => |state_id| {
+                    const State = fsm_state_collect.fsm_states[state_id].State;
+                    if (State == Exit) return null;
+                    const conthandler = State.conthandler;
+                    switch (conthandler(ctx)) {
+                        inline .no_trasition => return curr_id,
+                        inline .next => |wit0| {
+                            switch (wit0) {
+                                inline else => |wit, tag| {
+                                    _ = tag;
+                                    const key = Adler32.hash(@typeName(@TypeOf(wit)));
+                                    const new_id = fsm_state_collect.avl.search(fsm_state_collect.root, key).?;
+                                    return @as(StateId, @intCast(new_id));
+                                },
+                            }
+                        },
+                        inline .current => |wit0| {
+                            switch (wit0) {
+                                inline else => |wit, tag| {
+                                    _ = tag;
+                                    const key = Adler32.hash(@typeName(@TypeOf(wit)));
+                                    const new_id = fsm_state_collect.avl.search(fsm_state_collect.root, key).?;
+                                    continue :sw @as(StateId, @intCast(new_id));
+                                },
+                            }
+                        },
+                    }
+                },
+            }
+        }
+    };
+}
